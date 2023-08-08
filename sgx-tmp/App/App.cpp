e3f7cf74d5f2be9c758ce647b6096596223eb478
1uf3 <contact@lufe.jp>
2023-08-08 15:25:15 +0900

    sealing and unsealing

--- sgx-tmp/App/App.cpp
+++ sgx-tmp/App/App.cpp
@@ -14,6 +14,24 @@
 
 sgx_enclave_id_t global_eid = 0;
 
+
+void ocall_print_str(const char* str) {
+	std::cout << "Output from OCALL: " << std::endl;
+	std::cout << str << std::endl;
+}
+
+void ocall_print_size(size_t size) {
+	std::cout << size << std::endl;
+}
+
+void ocall_print_32size(uint32_t size) {
+	std::cout << size << std::endl;
+}
+
+void ocall_print_status(sgx_status_t s) {
+	print_sgx_status(s);
+}
+
 int initialize_enclave() {
 	sgx_launch_token_t token = {0};
 	sgx_status_t ret = SGX_SUCCESS;
@@ -23,6 +41,33 @@ int initialize_enclave() {
 	ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token, &updated, &global_eid, NULL);
 	if (ret != SGX_SUCCESS) {
 		printf("App: error %#x, failed to create enclave \n", ret);
+		return -1;
+	}
+    //0埋めしたダミーの起動トークンでEnclaveを作成する
+	return 0;
+}
+
+int initialize_enclave_ex() {
+	sgx_launch_token_t token = {0};
+	sgx_status_t ret = SGX_SUCCESS;
+	int updated = 0;
+
+	sgx_uswitchless_config_t us_config = SGX_USWITCHLESS_CONFIG_INITIALIZER;
+	void* enclave_ex_p[32] = {0};
+	enclave_ex_p[SGX_CREATE_ENCLAVE_EX_SWITCHLESS_BIT_IDX] = &us_config;
+
+	ret = sgx_create_enclave_ex(
+		ENCLAVE_FILENAME, 
+		SGX_DEBUG_FLAG, 
+		&token, 
+		&updated, 
+		&global_eid, 
+		NULL, 
+		SGX_CREATE_ENCLAVE_EX_SWITCHLESS, 
+		(const void**)enclave_ex_p
+		);
+	if (ret != SGX_SUCCESS) {
+		print_sgx_status(ret);
 		return -1;
 	}
     //0埋めしたダミーの起動トークンでEnclaveを作成する
@@ -40,25 +85,29 @@ int main() {
 	const char* message = "Hello Enclave.";
 	int retval = 0;
 	sgx_status_t ret = SGX_SUCCESS;
-	uint8_t sealed_data[DATA_SIZE] = {0};
+	uint8_t* sealed_data = new uint8_t[DATA_SIZE];
 	size_t sealed_data_size = 0;
 
-	ret = ecall_seal_data(global_eid, &retval, message, strlen(message), sealed_data, sealed_data_size);
+	ret = ecall_seal_data(global_eid, &retval, (uint8_t*)message, strlen(message), sealed_data, &sealed_data_size);
 	if (SGX_SUCCESS != ret) {
 		print_sgx_status(ret);
 		return -1;
 	}
 	printf("Returned integer from ECALL is: %d\n", retval);
 
-	char plain_data[DATA_SIZE] = {0};
-	size_t plain_data_size = 0;
+	BIO_dump_fp(stdout, (char*)sealed_data, sealed_data_size);
 
-	ret = ecall_unseal_data(global_eid, &retval, sealed_data, sealed_data_size, plain_data, plain_data_size);
+	uint8_t* plain_data = new uint8_t[DATA_SIZE];
+	uint32_t plain_data_size = 0;
+
+	ret = ecall_unseal_data(global_eid, &retval, sealed_data, sealed_data_size, plain_data, &plain_data_size);
 	if (SGX_SUCCESS != ret) {
 		print_sgx_status(ret);
 		return -1;
 	}
 	printf("Returned integer from ECALL is: %d\n", retval);
+
+	BIO_dump_fp(stdout, (char*)plain_data, plain_data_size);
 	 
 	if (SGX_SUCCESS != sgx_destroy_enclave(global_eid)) {
 		puts("App: error, failed to destroy enclave \n");
--- sgx-tmp/Enclave/Enclave.cpp
+++ sgx-tmp/Enclave/Enclave.cpp
@@ -1,47 +1,32 @@
 #include "Enclave_t.h"
 #include <sgx_trts.h>
 #include "sgx_tseal.h"
+#include <string.h>
 
-#define SEALED_DATA_SIZE 512
+#define SEALED_DATA_SIZE 1024 
 
-
-int ecall_seal_data(const char* plain_data_str, size_t plain_data_size, 
-                    uint8_t* sealed_data, size_t sealed_data_size) {
-    uint8_t* plain_data = (uint8_t*)atoi(plain_data_str);
-	sealed_data_size = sgx_calc_sealed_data_size(0, plain_data_size);
-    uint8_t sealed_data_array[sealed_data_size];
+int ecall_seal_data(uint8_t* plain_data, size_t plain_data_size, 
+                    uint8_t* sealed_data, size_t* sealed_data_size) {
+    ocall_print_str((char*)plain_data);
+	*sealed_data_size = sgx_calc_sealed_data_size(0, plain_data_size);
+    ocall_print_size(*sealed_data_size);
 	sgx_status_t status = sgx_seal_data(
 		0, 
 		NULL, 
 		plain_data_size, 
 		plain_data,
-		sealed_data_size,
+		*sealed_data_size,
 		(sgx_sealed_data_t*)sealed_data
         );
-    sealed_data = sealed_data_array;
+    ocall_print_status(status);
     return 0;
 }
 
-int ecall_unseal_data(uint8_t* sealed_data_u, size_t sealed_data_size,
-                        char* plain_data, size_t plain_data_size) {
-    sgx_sealed_data_t* sealed_data = (sgx_sealed_data_t*)sealed_data_u;
-    plain_data_size = sgx_get_encrypt_txt_len(sealed_data);
-    uint8_t* plain_data_u = new uint8_t[plain_data_size]();
-    sgx_status_t status = sgx_unseal_data(sealed_data, NULL, 0, plain_data_u, (uint32_t*)plain_data_size);
-    plain_data = (char*)plain_data_u;
+int ecall_unseal_data(uint8_t* sealed_data, size_t sealed_data_size,
+                        uint8_t* plain_data, uint32_t* plain_data_size) {
+    *plain_data_size = sgx_get_encrypt_txt_len((sgx_sealed_data_t*)sealed_data);
+    ocall_print_32size(*plain_data_size);
+    sgx_status_t status = sgx_unseal_data((sgx_sealed_data_t*)sealed_data, NULL, 0, plain_data, plain_data_size);
+    ocall_print_status(status);
     return 0;
 }
-
-// int ecall_seal_data(const char* plain_data, size_t plain_data_size) {
-//     uint8_t sealed_data[SEALED_DATA_SIZE] = {0};
-//     size_t sealed_data_size = 0;
-//     sgx_status_t status = seal_data(plain_data, plain_data_size, sealed_data, &sealed_data_size);
-//     return 0;
-// }
-// 
-// int ecall_unseal_data(const uint8_t* sealed_data, size_t sealed_data_size) {
-//     uint8_t plain_data[SEALED_DATA_SIZE] = {0};
-//     size_t plain_data_size = 0;
-//     sgx_status_t status = unseal_data(sealed_data, sealed_data_size, plain_data, &plain_data_size);
-//     return 0;
-// }
