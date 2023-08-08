690378dab8f2f7c10a9e8bb5f104dc9c20e2c537
1uf3 <contact@lufe.jp>
2023-08-08 12:33:31 +0900

    failed

--- sgx-tmp/App/App.cpp
+++ sgx-tmp/App/App.cpp
@@ -10,67 +10,19 @@
 
 #define ENCLAVE_FILENAME "enclave.signed.so"
 #define N 1000000
+#define DATA_SIZE 1024
 
 sgx_enclave_id_t global_eid = 0;
-
-void ocall_print(const char* str) {
-	std::cout << "Output from OCALL: " << std::endl;
-	std::cout << str << std::endl;
-}
-
-void ocall_print_ex(const char* str) {
-	std::cout << "Output from OCALL: " << std::endl;
-	std::cout << str << std::endl;
-}
 
 int initialize_enclave() {
 	sgx_launch_token_t token = {0};
 	sgx_status_t ret = SGX_SUCCESS;
 	int updated = 0;
 
-	sgx_uswitchless_config_t us_config = SGX_USWITCHLESS_CONFIG_INITIALIZER;
-	void* enclave_ex_p[32] = {0};
-	enclave_ex_p[SGX_CREATE_ENCLAVE_EX_SWITCHLESS_BIT_IDX] = &us_config;
 
-	ret = sgx_create_enclave_ex(
-		ENCLAVE_FILENAME, 
-		SGX_DEBUG_FLAG, 
-		&token, 
-		&updated, 
-		&global_eid, 
-		NULL, 
-		SGX_CREATE_ENCLAVE_EX_SWITCHLESS, 
-		(const void**)enclave_ex_p
-		);
+	ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token, &updated, &global_eid, NULL);
 	if (ret != SGX_SUCCESS) {
-		print_sgx_status(ret);
-		return -1;
-	}
-    //0埋めしたダミーの起動トークンでEnclaveを作成する
-	return 0;
-}
-
-int initialize_enclave_ex() {
-	sgx_launch_token_t token = {0};
-	sgx_status_t ret = SGX_SUCCESS;
-	int updated = 0;
-
-	sgx_uswitchless_config_t us_config = SGX_USWITCHLESS_CONFIG_INITIALIZER;
-	void* enclave_ex_p[32] = {0};
-	enclave_ex_p[SGX_CREATE_ENCLAVE_EX_SWITCHLESS_BIT_IDX] = &us_config;
-
-	ret = sgx_create_enclave_ex(
-		ENCLAVE_FILENAME, 
-		SGX_DEBUG_FLAG, 
-		&token, 
-		&updated, 
-		&global_eid, 
-		NULL, 
-		SGX_CREATE_ENCLAVE_EX_SWITCHLESS, 
-		(const void**)enclave_ex_p
-		);
-	if (ret != SGX_SUCCESS) {
-		print_sgx_status(ret);
+		printf("App: error %#x, failed to create enclave \n", ret);
 		return -1;
 	}
     //0埋めしたダミーの起動トークンでEnclaveを作成する
@@ -87,47 +39,31 @@ int main() {
 
 	const char* message = "Hello Enclave.";
 	int retval = 0;
-	sgx_status_t ret_ex = SGX_SUCCESS;
-
-	std::chrono::system_clock::time_point  start_ex, end_ex;
-	start_ex = std::chrono::system_clock::now(); // 計測開始時間
-	for (int i=0; i<N; i++) {
-		ret_ex = ecall_test_ex(global_eid, &retval, message, strlen(message));
-	}
- 	end_ex = std::chrono::system_clock::now();  // 計測終了時間
- 	double elapsed_ex = std::chrono::duration_cast<std::chrono::milliseconds>(end_ex-start_ex).count(); //処理に要した時間をミリ秒に変換
-	printf("uSwitchless time: %lf\n", elapsed_ex);
-
-	print_sgx_status(ret_ex);
-	printf("Returned integer from ECALL is: %d\n", retval);
-	 
-	if (SGX_SUCCESS != sgx_destroy_enclave(global_eid)) {
-		printf("App: error %#x, failed to destroy enclave \n", ret_ex);
-		return -1;
-	}
-
-	// =========== //
-
-	retval = 0;
 	sgx_status_t ret = SGX_SUCCESS;
+	uint8_t sealed_data[DATA_SIZE] = {0};
+	size_t sealed_data_size = 0;
 
-	initialize_enclave();
-	std::chrono::system_clock::time_point  start, end; 
-	start = std::chrono::system_clock::now(); // 計測開始時間
-	for (int i=0; i<N; i++) {
-		ret = ecall_test(global_eid, &retval, message, strlen(message));
+	ret = ecall_seal_data(global_eid, &retval, message, strlen(message), sealed_data, sealed_data_size);
+	if (SGX_SUCCESS != ret) {
+		print_sgx_status(ret);
+		return -1;
 	}
- 	end = std::chrono::system_clock::now();  // 計測終了時間
- 	double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
-	printf("non-uSwitchless time: %lf\n", elapsed);
+	printf("Returned integer from ECALL is: %d\n", retval);
 
-	print_sgx_status(ret);
+	char plain_data[DATA_SIZE] = {0};
+	size_t plain_data_size = 0;
+
+	ret = ecall_unseal_data(global_eid, &retval, sealed_data, sealed_data_size, plain_data, plain_data_size);
+	if (SGX_SUCCESS != ret) {
+		print_sgx_status(ret);
+		return -1;
+	}
 	printf("Returned integer from ECALL is: %d\n", retval);
 	 
 	if (SGX_SUCCESS != sgx_destroy_enclave(global_eid)) {
-		printf("App: error %#x, failed to destroy enclave \n", ret);
+		puts("App: error, failed to destroy enclave \n");
 		return -1;
 	}
-	 
-	 return 0;
+
+	return 0;
 }
--- sgx-tmp/Enclave/Enclave.cpp
+++ sgx-tmp/Enclave/Enclave.cpp
@@ -1,12 +1,47 @@
 #include "Enclave_t.h"
 #include <sgx_trts.h>
+#include "sgx_tseal.h"
 
-int ecall_test(const char* message, size_t message_len) {
-    // ocall_print(message);
-    return 19;
+#define SEALED_DATA_SIZE 512
+
+
+int ecall_seal_data(const char* plain_data_str, size_t plain_data_size, 
+                    uint8_t* sealed_data, size_t sealed_data_size) {
+    uint8_t* plain_data = (uint8_t*)atoi(plain_data_str);
+	sealed_data_size = sgx_calc_sealed_data_size(0, plain_data_size);
+    uint8_t sealed_data_array[sealed_data_size];
+	sgx_status_t status = sgx_seal_data(
+		0, 
+		NULL, 
+		plain_data_size, 
+		plain_data,
+		sealed_data_size,
+		(sgx_sealed_data_t*)sealed_data
+        );
+    sealed_data = sealed_data_array;
+    return 0;
 }
 
-int ecall_test_ex(const char* message, size_t message_len) {
-    // ocall_print(message);
-    return 20;
+int ecall_unseal_data(uint8_t* sealed_data_u, size_t sealed_data_size,
+                        char* plain_data, size_t plain_data_size) {
+    sgx_sealed_data_t* sealed_data = (sgx_sealed_data_t*)sealed_data_u;
+    plain_data_size = sgx_get_encrypt_txt_len(sealed_data);
+    uint8_t* plain_data_u = new uint8_t[plain_data_size]();
+    sgx_status_t status = sgx_unseal_data(sealed_data, NULL, 0, plain_data_u, (uint32_t*)plain_data_size);
+    plain_data = (char*)plain_data_u;
+    return 0;
 }
+
+// int ecall_seal_data(const char* plain_data, size_t plain_data_size) {
+//     uint8_t sealed_data[SEALED_DATA_SIZE] = {0};
+//     size_t sealed_data_size = 0;
+//     sgx_status_t status = seal_data(plain_data, plain_data_size, sealed_data, &sealed_data_size);
+//     return 0;
+// }
+// 
+// int ecall_unseal_data(const uint8_t* sealed_data, size_t sealed_data_size) {
+//     uint8_t plain_data[SEALED_DATA_SIZE] = {0};
+//     size_t plain_data_size = 0;
+//     sgx_status_t status = unseal_data(sealed_data, sealed_data_size, plain_data, &plain_data_size);
+//     return 0;
+// }
